c1e41c34a875414c1d99b290f33e530ca3076638
Rob Ellingson <76190447+awolrob@users.noreply.github.com>
2021-06-06 11:28:44 -0400

    set up js structure; var to match psedoCode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,35 @@
-// Assignment code here
-
+/* FUNCTIONS  */
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
+  var password = passReqs.passResult();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
+/* END FUNCTIONS */
+
+/* INFORMATION / VARIABLES */
+var passReqs = {
+    lowerCase : false,
+    upperCase : false,
+    num : false,
+    specChar : false,
+    passResult : function () {
+    return "yeah! - I made it into the textarea"
+  }
+}
+
+/* INFORMATION / VARIABLES */
+
+/* MAIN LOGC */
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
