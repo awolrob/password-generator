d27156def553b8d17161d57a91257fcf5d90e2d1
Rob Ellingson <76190447+awolrob@users.noreply.github.com>
2021-06-06 11:42:52 -0400

    prompt and edit pass len

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,25 @@
 /* FUNCTIONS  */
 
+var getLen = function () {
+  var rLen = 0;
+
+  while (rLen > 128 || rLen < 8) {
+    rLen = window.prompt("Enter desired password length ( min: 8 / max: 128 )");
+    if (rLen > 128) {
+      window.alert("Sorry - the length cannot exceed 128.  \n\nPlease try again.")
+    }
+    if (rLen < 8) {
+      window.alert("Sorry - the length must be at least 8.  \n\nPlease try again.")
+    }
+  }
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  // var password = generatePassword();
+  var len = getLen();
   var password = passReqs.passResult();
   var passwordText = document.querySelector("#password");
 
@@ -17,11 +31,11 @@ function writePassword() {
 
 /* INFORMATION / VARIABLES */
 var passReqs = {
-    lowerCase : false,
-    upperCase : false,
-    num : false,
-    specChar : false,
-    passResult : function () {
+  lowerCase: false,
+  upperCase: false,
+  num: false,
+  specChar: false,
+  passResult: function () {
     return "yeah! - I made it into the textarea"
   }
 }
