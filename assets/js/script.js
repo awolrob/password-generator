dda6fa095483ffc2c750695bfda8171e8bd504c3
Rob Ellingson <76190447+awolrob@users.noreply.github.com>
2021-06-06 21:18:57 -0400

    added password char list; generated password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 /* FUNCTIONS  */
-
 var getLen = function () {
   var rLen = "";
-  debugger;
   while (rLen > 128 || rLen < 8 || isNaN(rLen)) {
     rLen = window.prompt("Enter desired password length ( min: 8 / max: 128 )");
 
@@ -12,7 +10,7 @@ var getLen = function () {
       if (rLen > 128) {
         window.alert("Sorry - the length cannot exceed 128.  \n\nPlease try again.")
       }
-      if (rLen < 8 ) {
+      if (rLen < 8) {
         window.alert("Sorry - the length must be at least 8.  \n\nPlease try again.")
       }
     }
@@ -21,23 +19,43 @@ var getLen = function () {
 };
 
 var getCharTypes = function () {
-  passReqs.lowerCase = window.confirm("Do you want lower case letters?");
-  console.log(passReqs.lowerCase);
-
-  passReqs.upperCase = window.confirm("Do you want UPPER case letters?");
-  console.log(passReqs.upperCase);
-
-  passReqs.num = window.confirm("Do you want numbers?");
-  console.log(passReqs.num);
-
-  passReqs.specChar = window.confirm("Do you want special characters?");
-  console.log(passReqs.specChar);
-
+  passReqs.lowerCase = window.confirm("Do you want lower case letters in your password?");
+  passReqs.upperCase = window.confirm("Do you want UPPER case letters in your password?");
+  passReqs.num = window.confirm("Do you want numbers in your password?");
+  passReqs.specChar = window.confirm("Do you want special characters in your password?");
 };
 
-var generatePassword= function () {
-  return "yeah! - I made it into the textarea " + passReqs.len;
-}
+var generatePassword = function () {
+
+  var selPassCharSet = " ";
+  var retPassword = "\"";
+
+  if (passReqs.lowerCase) {
+    selPassCharSet = selPassCharSet + "abcdefghijklmnopqrstuvwxyz";
+  }
+  if (passReqs.upperCase) {
+    selPassCharSet = selPassCharSet + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  if (passReqs.num) {
+    selPassCharSet = selPassCharSet + "0123456789";
+  }
+
+  if (passReqs.specChar) {
+    selPassCharSet = selPassCharSet + ' !\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+  }
+  
+  for (var iSelPassLen = 0; iSelPassLen < passReqs.len; iSelPassLen++) {
+    // randomly select character from selPassCharSet
+    retPassword = selPassCharSet[randomNumber(0, selPassCharSet.length)] + retPassword;
+  }
+  // return "yeah! - I made it into the textarea " + passReqs.len;
+  return "\"" + retPassword;
+};
+// generate random number for use in selPassCharSet (selected password character set)
+var randomNumber = function (min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+  return value;
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -46,9 +64,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = passReqs.passResult();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 /* END FUNCTIONS */
@@ -63,16 +79,16 @@ var passReqs = {
   passResult: function () {
     passReqs.len = getLen();
     passGetTypes = getCharTypes();
-    console.log(passReqs.len);
     return generatePassword();
   }
-}
+};
 
 /* END INFORMATION / VARIABLES */
 
 /* MAIN LOGC */
 
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
 
 /* END MAIN LOGIC */
